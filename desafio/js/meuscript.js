078ca7dbf20efd6b61775978d19a8dfc8243eff0
Alexandre Garcia Aguado <aleaguado@users.noreply.github.com>
2024-09-19 12:04:11 -0300

    Delete desafio directory

--- desafio/js/meuscript.js
+++
@@ -1,9 +0,0 @@
-function lamp(){
-    var objeto = document.getElementById("lamp");
-    
-    if (objeto.src.match("off")) {
-        objeto.src = "img/on.png";
-    } else {
-        objeto.src = "img/off.png";
-    }
-}
